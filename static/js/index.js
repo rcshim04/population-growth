13271e0b0f91aeb64b2cb22a9facd12dabc4fdf2
rcshim04 <rcshim04@gmail.com>
2022-01-26 01:33:49 -0500

    Change to hsv

--- static/js/index.js
+++ static/js/index.js
@@ -40,8 +40,8 @@ function changeTheme() {
     $(':root').css('--theme', $('#theme').val());
     var rgb = hexToRgb($('#theme').val());
     if (rgb != null) {
-        var hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
-        if (hsl[2] > 0.5) {
+        var hsv = rgbToHsv(rgb[0], rgb[1], rgb[2]);
+        if (hsv[2] > 0.5) {
             $(':root').css('--bg', '#202124');
         } else {
             $(':root').css('--bg', '#ffffff');
@@ -65,26 +65,25 @@ function hexToRgb(h) {
     return [ +r, +g, +b ];
   }
 
-function rgbToHsl(r, g, b) {
+  function rgbToHsv(r, g, b) {
     r /= 255, g /= 255, b /= 255;
   
     var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
+    var h, s, v = max;
+  
+    var d = max - min;
+    s = max == 0 ? 0 : d / max;
   
     if (max == min) {
-        h = s = 0;
+        h = 0;
     } else {
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-    
         switch (max) {
             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
             case g: h = (b - r) / d + 2; break;
             case b: h = (r - g) / d + 4; break;
         }
-    
         h /= 6;
     }
   
-    return [ h, s, l ];
-  }
+    return [ h, s, v ];
+}
