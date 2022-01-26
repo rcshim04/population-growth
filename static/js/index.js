0b4beaf145f1a3fd6ba9a5271ed65ac45b7854e0
rcshim04 <rcshim04@gmail.com>
2022-01-26 01:20:26 -0500

    Implemented dark mode

--- static/js/index.js
+++ static/js/index.js
@@ -38,4 +38,53 @@ function onlyNumberKey(evt) {
 
 function changeTheme() {
     $(':root').css('--theme', $('#theme').val());
+    var rgb = hexToRgb($('#theme').val());
+    if (rgb != null) {
+        var hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
+        if (hsl[2] > 0.5) {
+            $(':root').css('--bg', '#202124');
+        } else {
+            $(':root').css('--bg', '#ffffff');
+        }
+    }
 }
+
+function hexToRgb(h) {
+    let r = 0, g = 0, b = 0;
+  
+    if (h.length == 4) {
+        r = "0x" + h[1] + h[1];
+        g = "0x" + h[2] + h[2];
+        b = "0x" + h[3] + h[3];
+    } else if (h.length == 7) {
+        r = "0x" + h[1] + h[2];
+        g = "0x" + h[3] + h[4];
+        b = "0x" + h[5] + h[6];
+    }
+    
+    return [ +r, +g, +b ];
+  }
+
+function rgbToHsl(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+  
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+  
+    if (max == min) {
+        h = s = 0;
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+    
+        h /= 6;
+    }
+  
+    return [ h, s, l ];
+  }
